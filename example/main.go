50e4e5a82ca41da7ac105db8cd7d92a2bf85c406
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-09-27 16:13:32 -0300

    FIX: problema da ausencia de retorno quando a lingua é alterada

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	goerror "github.com/rantool-team/go-error"
+	"github.com/rantool-team/go-error/language"
 )
 
 func main() {
@@ -9,5 +10,8 @@ func main() {
 	e := goerror.CreateError("The world is ungly", "none")
 	e.SetContext("", "", "not-a-language")
 	e.SetStatusCode(220)
+	e.SetLanguage(language.PORTUGUESE_STRING)
+	e.AddDescriptionInOtherLanguage(language.PORTUGUESE_STRING, "portuguê")
+	e.AddMessageInOtherLanguage(language.PORTUGUESE_STRING, "português")
 	e.Emit()
 }
--- language/set-message.go
+++ language/set-message.go
@@ -4,6 +4,7 @@ func (m *MessageSet) SetMessage(languageName string, newMessage string) {
 	languageId, ok := getLanguageIotaAndReturnIfHasPredefinedLanguage(languageName)
 	if ok {
 		m.setMessageOnId(languageId, newMessage)
+		return
 	}
 
 	m.setInAnotherLanguage(languageName, newMessage)
@@ -13,44 +14,64 @@ func (m *MessageSet) setMessageOnId(id Language, newMessage string) {
 	switch id {
 	case ENGLISH:
 		m.English = newMessage
+		return
 	case PORTUGUESE:
 		m.Portuguese = newMessage
+		return
 	case SPANISH:
 		m.Spanish = newMessage
+		return
 	case MANDARIN_CHINESE:
 		m.MandarinChinese = newMessage
+		return
 	case HINDI:
 		m.Hindi = newMessage
+		return
 	case FRENCH:
 		m.French = newMessage
+		return
 	case ARABIC:
 		m.Arabic = newMessage
+		return
 	case BENGALI:
 		m.Bengali = newMessage
+		return
 	case RUSSIAN:
 		m.Russian = newMessage
+		return
 	case URDU:
 		m.Urdu = newMessage
+		return
 	case INDONESIAN:
 		m.Indonesian = newMessage
+		return
 	case GERMAN:
 		m.German = newMessage
+		return
 	case JAPANESE:
 		m.Japanese = newMessage
+		return
 	case SWAHILI:
 		m.Swahili = newMessage
+		return
 	case PUNJABI:
 		m.Punjabi = newMessage
+		return
 	case MARATHI:
 		m.Marathi = newMessage
+		return
 	case TELUGU:
 		m.Telugu = newMessage
+		return
 	case TURKISH:
 		m.Turkish = newMessage
+		return
 	case KOREAN:
 		m.Korean = newMessage
+		return
 	case VIETNAMESE:
 		m.Vietnamese = newMessage
+		return
 	}
 
 	m.SetMessage(defaultLanguage, newMessage)
