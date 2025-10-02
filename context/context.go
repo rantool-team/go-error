1431cec48306c6a3f58a07ce334882945da7c522
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-10-02 15:46:39 -0300

    Criação de uma ferramenta de indicações de locais

--- context/context.go
+++ context/context.go
@@ -4,4 +4,16 @@ type Context struct {
 	Description      string
 	LocalDescription string
 	Language         string
+	Local            Local
+}
+
+type Local struct {
+	StartLine int
+	EndLine   int
+	StartChar int
+	EndChar   int
+
+	Package   string
+	Module    string
+	Workspace string
 }
